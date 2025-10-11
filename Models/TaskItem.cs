db2828260a6958a4f28efba109cc80beb9e80ee9
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-11 13:36:53 +0545

    Fixing error

--- Models/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalAssistantAI.Models;
-
-public class TaskItem
-{
-    public int Id { get; set; }
-    public string? Description { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public bool IsCompleted { get; set; }
-}
